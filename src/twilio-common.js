cc402fdf4b122ac5b53a8bcaa4ca794c709d8f41
Mark Andrus Roberts <mroberts@twilio.com>
2015-11-03 17:42:51 -0800

    Initial commit

---
+++ src/twilio-common.js
@@ -0,0 +1,16 @@
+/*! ${name}.js ${version}
+
+#include "LICENSE.md"
+ */
+/* eslint strict:0 */
+(function(root) {
+  var component = require('./twilio-common-bundle');
+  if (typeof define === 'function' && define.amd) {
+    define([], function() { return component; });
+  } else {
+    root.Twilio = root.Twilio || function Twilio() {};
+    for (var componentName in component) {
+      root.Twilio[componentName] = component[componentName];
+    }
+  }
+})(window || global || this);
